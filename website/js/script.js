4ebb3c58dd9db4916c9de9f4dcd463dfe8b27d50
naimishagirish <134681701+naimishagirish@users.noreply.github.com>
2023-07-28 20:06:06 +0400

    Update script.js

--- website/js/script.js
+++ website/js/script.js
@@ -1,48 +1,6 @@
-/*$(function(){ // Same as document.addEventListener("DOMContentLoaded"..
-
-	$("#btn").blur(function(event){  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-		var screenWidth=window.innerWidth;
-		if(screenWidth<768){
-			$("#collapsable-nav").collapse('hide');
-		}
-	}); 
-
-	$("#btn").click(function (event) {
-    $(event.target).focus();
+$(window).on('click', function(event){
+    var clickOver = $(event.target)
+    if ($('.navbar .navbar-toggle').attr('aria-expanded') == 'true' && clickOver.closest('.navbar').length === 0) {
+        $('button[aria-expanded="true"]').click();
+    }
 });
-
-});*/
-
-
-(function(global){
-
-	var dc={};
-
-	var homeHtml="snippets/home-snippet.html";
-
-	var insertHtml=function(selector,html){
-		var targetElem=document.querySelector(selector);
-		targetElem.innerHTML=html;
-	}
-
-	var showLoading=function(selector){
-		var html="div class='tex-center'>";
-		html+="<img src='images/spin.gif'></div>";
-		insertHtml(selector,html);
-	}
-
-
-
-document.addEventListener("DOMContentLoaded",function(event){
-	showLoading("#main-content");
-
-	$ajaxUtils.sendGetRequest(
-		homeHtml, function(responseText){
-			document.querySelector("#main-content").innerHTML=responseText;
-		},false);
-});
-
-
-
-	global.$dc=dc;
-})(window);
